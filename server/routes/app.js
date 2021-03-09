27e8e39dd406df2b90f5b280ba04669168a18c21
Holt Picklesimer <holtpicklesimer@gmail.com>
2021-03-09 11:03:30 -0700

    Path already exists.

--- server/routes/app.js
+++ server/routes/app.js
@@ -1,11 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const path = require("path");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.sendFile(path.join(__dirname, "dist/cms/index.html"));
-  // res.render("index", { title: "CMS" });
 });
 
 module.exports = router;
